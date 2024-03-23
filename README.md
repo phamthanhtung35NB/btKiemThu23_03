6d7b3344872a509f4cd261b44954a631b0f15060
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-03-23 17:01:12 +0700

    kiem tra 3 kich thuoc nhap vao co phai la 3 canh cua tam giac khong

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Phạm Thanh Tùng - 22021128
+# Phạm Thanh Tùng - 22021128
+### Kiểm tra ba số a, b, c có là cạnh của một tam giác không
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,32 @@
 //Pham Thanh Tung _ 22021128
 //kiem tra 3 kich thuoc nhap vao co phai la 3 canh cua tam giac khong
 import java.util.Scanner;
+
 public class Main {
 
-  float a, b, c;
-
+  public boolen checkTriangle(float a, float b, float c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      System.out.println("3 canh tam giac phai lon hon 0");
+      return 0;
+    }else{
+      if (a + b > c && a + c > b && b + c > a) {
+        return 1;
+      } else {
+        System.out.println(a+"; "+b+"; "+c+"; Khong hong phai la 3 canh cua tam giac");
+        return 0;
+      }
+    }
+  }
 
   public static void main(String[] args) {
+    float a, b, c;
     Scanner sc = new Scanner(System.in);
     System.out.println("Nhap 3 canh cua tam giac: ");
     float a = sc.nextFloat();
     float b = sc.nextFloat();
     float c = sc.nextFloat();
-    if (a <= 0 || b <= 0 || c <= 0) {
-      System.out.println("3 canh tam giac phai lon hon 0");
-      return;
-    }else{
-      if (a + b > c && a + c > b && b + c > a) {
-        System.out.println(a+"; "+b+"; "+c+"; La 3 canh cua tam giac");
-      } else {
-        System.out.println(a+"; "+b+"; "+c+"; Khong hong phai la 3 canh cua tam giac");
-      }
+    if(checkTriangle(a, b, c)){
+      System.out.println(a+"; "+b+"; "+c+"; La 3 canh cua tam giac");
     }
 
   }

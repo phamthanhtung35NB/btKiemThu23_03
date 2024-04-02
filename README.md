b75b2bf8e8ea1680ac30541fc93dba57c22e557a
Phạm Thanh Tùng <tungpham010203@gmail.com>
2024-04-02 23:44:31 +0700

    lam lai

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Phạm Thanh Tùng - 22021128
-### Kiểm tra ba số a, b, c có là cạnh của một tam giác không
--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,32 @@
-//Pham Thanh Tung _ 22021128
-//kiem tra 3 kich thuoc nhap vao co phai la 3 canh cua tam giac khong
 import java.util.Scanner;
-
 public class Main {
 
-  static boolean checkTriangle(float a, float b, float c) {
-    if (a <= 0 || b <= 0 || c <= 0) {
-      System.out.println("3 canh tam giac phai lon hon 0");
-      return false;
-    }else{
-      if (a + b > c && a + c > b && b + c > a) {
-        return true;
-      } else {
-        System.out.println(a+"; "+b+"; "+c+"; Khong hong phai la 3 canh cua tam giac");
-        return false;
-      }
-    }
-  }
-
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Nhap 3 canh cua tam giac: ");
-    float a = sc.nextFloat();
-    float b = sc.nextFloat();
-    float c = sc.nextFloat();
-    if(checkTriangle(a, b, c)){
-      System.out.println(a+"; "+b+"; "+c+"; La 3 canh cua tam giac");
+    Scanner scanner = new Scanner(System.in);
+    int vt = scanner.nextInt();
+    int d = scanner.nextInt();
+
+    if (vt <= 0 || d <= 0 || vt > 120) {
+      System.out.println("input khong hop le");
+      return;
     }
 
+    // Xác định khoảng cách an toàn tối thiểu theo vận tốc
+    int khoangCachAnToan = 0;
+    if (vt == 60) {
+      khoangCachAnToan = 35;
+    } else if (vt <= 80) {
+      khoangCachAnToan = 55;
+    } else if (vt <= 100) {
+      khoangCachAnToan = 70;
+    } else if (vt <= 120) {
+      khoangCachAnToan = 100;
+    }
+    if (d < khoangCachAnToan) {
+      System.out.println("Cảnh báo");
+      return;
+    }
+    System.out.println("An toàn");
+    return;
   }
-
-
 }
